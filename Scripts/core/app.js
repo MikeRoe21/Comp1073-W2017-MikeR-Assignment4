58043a6d8b6588202f023a1e13cd44cc83a70ec9
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2017-04-18 15:04:09 -0400

    added label and button ts/js/js.map

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -1,11 +1,39 @@
 (function () {
+    var canvas;
+    var stage;
+    var Test;
+    /**
+     *
+     * @method Start
+     * @returns void
+     */
     function Start() {
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
         Main();
     }
-    function Update() {
+    /**
+     * @method Update
+     * @returns void
+     */
+    function Update(event) {
+        Test.rotation += 2;
+        stage.update();
     }
+    /**
+     * @method Main
+     * @returns void
+     */
     function Main() {
         console.log("App Started!!");
+        Test = new createjs.Text("My Projects", "24px Verdana", "#000000");
+        Test.regX = Test.getMeasuredWidth() * 0.5;
+        Test.regY = Test.getMeasuredHeight() * 0.5;
+        Test.x = 234;
+        Test.y = 30;
+        stage.addChild(Test);
     }
     window.onload = Start;
 })();
--- Scripts/core/app.ts
+++ Scripts/core/app.ts
@@ -1,16 +1,52 @@
 (function(){
 
+let canvas:HTMLElement;
+let stage:createjs.Stage;
+let Test:createjs.Text;
+
+/**
+ * 
+ * @method Start
+ * @returns void
+ */
 function Start(){
+
+        canvas = document.getElementById("canvas");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+
+
     Main();
 }
 
-function Update() {
+/**
+ * @method Update
+ * @returns void
+ */
+function Update(event:createjs.Event):void {
 
+    Test.rotation += 2;
+    
+   
+
+    stage.update();
 
 }
 
-function Main() {
+/**
+ * @method Main
+ * @returns void
+ */
+function Main():void {
     console.log("App Started!!");
+
+    Test = new createjs.Text("My Projects", "24px Verdana","#000000");
+    Test.regX = Test.getMeasuredWidth() * 0.5;
+    Test.regY = Test.getMeasuredHeight() * 0.5;
+    Test.x = 234;
+    Test.y = 30;
+    stage.addChild(Test);
 }
 
 window.onload = Start;
